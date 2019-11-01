6d481a5b2b2f28a600db8aeafb28c983f48dd956
phongtiger <phongken94@gmail.com>
2019-11-01 22:03:10 +0700

    tailen

---
+++ src/app/date-util.service.spec.ts
@@ -0,0 +1,12 @@
+import {inject, TestBed} from '@angular/core/testing';
+
+import { DateUtilService } from './date-util.service';
+
+
+describe('DateUtilService', () => {
+  beforeEach(() => TestBed.configureTestingModule({providers: [DateUtilService]}));
+
+  it('should be created', inject([DateUtilService], (service: DateUtilService) => {
+    expect(service).toBeTruthy();
+  }));
+});
